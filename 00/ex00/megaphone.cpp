c223b93c09ba66ed406086bbba2c9842ccbeca83
mariosrb <mario-2000@hotmail.fr>
2025-08-28 11:23:48 +0200

    ajout parsing

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main (void) {
+
+	char	buff[512];
+
+	std::cout << "Hello word" << std::endl;
+
+	std::cout << "Input a word: ";
+	std::cin >> buff;
+	std::cout << "You entered: [" << buff << "]" << std::endl;
+	return (0);
+}
