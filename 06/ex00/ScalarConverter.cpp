2db81e282a13f0c4732aec56c5aa4dba789139b6
mariosrb <mario-2000@hotmail.fr>
2025-10-13 18:18:25 +0200

    Display done

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -13,7 +13,10 @@ void ScalarConverter::convert(std::string literal) {
 			int i = static_cast<int>(c);
 			float f = static_cast<float>(c);
 			double d = static_cast<double>(c);
-			std::cout << "char: '" << c << "'" << std::endl;
+			if (std::isprint(c))
+				std::cout << "char: '" << c << "'" << std::endl;
+			else
+				std::cout << "char: Non displayable" << std::endl;
 			std::cout << "int: " << i << std::endl;
 			std::cout << std::fixed << std::setprecision(1);
 			std::cout << "float: " << f << "f" << std::endl;
@@ -26,6 +29,14 @@ void ScalarConverter::convert(std::string literal) {
 			char c = static_cast<char>(i);
 			float f = static_cast<float>(i);
 			double d = static_cast<double>(i);
+			if (std::isprint(c))
+				std::cout << "char: '" << c << "'" << std::endl;
+			else
+				std::cout << "char: Non displayable" << std::endl;
+			std::cout << "int: " << i << std::endl;
+			std::cout << std::fixed << std::setprecision(1);
+			std::cout << "float: " << f << "f" << std::endl;
+			std::cout << "double: " << d << std::endl;
 			break;
 		}
 		case FLOAT:
@@ -34,6 +45,14 @@ void ScalarConverter::convert(std::string literal) {
 			char c = static_cast<char>(f);
 			double d = static_cast<double>(f);
 			int i = static_cast<int>(f);
+			if (std::isprint(c))
+				std::cout << "char: '" << c << "'" << std::endl;
+			else
+				std::cout << "char: Non displayable" << std::endl;
+			std::cout << "int: " << i << std::endl;
+			std::cout << std::fixed << std::setprecision(1);
+			std::cout << "float: " << f << "f" << std::endl;
+			std::cout << "double: " << d << std::endl;
 			break;
 		}
 		case DOUBLE:
@@ -42,6 +61,14 @@ void ScalarConverter::convert(std::string literal) {
 			int i = static_cast<int>(d);
 			float f = static_cast<float>(d);
 			char c = static_cast<char>(d);
+			if (std::isprint(c))
+				std::cout << "char: '" << c << "'" << std::endl;
+			else
+				std::cout << "char: Non displayable" << std::endl;
+			std::cout << "int: " << i << std::endl;
+			std::cout << std::fixed << std::setprecision(1);
+			std::cout << "float: " << f << "f" << std::endl;
+			std::cout << "double: " << d << std::endl;
 			break;
 		}
 		case PSEUDO_FLOAT:
