843fcd32bb4b36df8e6f7e2fda51f9cd4f5a1581
mariosrb <mario-2000@hotmail.fr>
2025-11-25 10:19:58 +0100

    commentaire supp

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -4,16 +4,11 @@ int main(int argc, char* argv[]) {
 	PmergeMe sorter;
 
 	try {
-		// On essaie d'exécuter le parsing
 		sorter.parseInput(argc, argv);
 		sorter.execute();
-		// ... le reste de ta logique de tri ...
-
 	} catch (const std::exception& e) {
-		// Si une exception est lancée (invalid_argument ou overflow_error)
-		// On l'attrape ici et on affiche le message d'erreur
 		std::cerr << e.what() << std::endl;
-		return (1); // On quitte le programme proprement avec un code d'erreur
+		return (1);
 	}
 
 	return (0);
